1cf41d4fce4aa78239f355e8e567d7a9e980e22d
Jason Showalter <jyshowalter@gmail.com>
2024-05-14 12:16:47 -0700

    added base game grid with basic styling

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Determine whose turn it is
+
+//Listen for a click in a box 
+
+//Depending on turn, place X for player = 1, place O for player = 2
+
+//Remove click ability for used box
+
+//Change players 
+
+//Resume play until 3 in a row 
+
+//Update to determine winner
+
+const winConditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
+
+let options = ['','','','','','','','','']
+
